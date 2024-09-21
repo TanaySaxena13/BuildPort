4b4af8391d89713d750df1be10566b3f0468e047
TanaySaxena13 <tanaysaxena661@gmail.com>
2024-09-21 22:09:59 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,37 +7,27 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    // Uncommented block to pass the first stage
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;  // Port to listen on
-
-    try {
-      serverSocket = new ServerSocket(port);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
-      serverSocket.setReuseAddress(true);
-      System.out.println("Server is listening on port " + port);
-      
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
-      System.out.println("Client connected");
-
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
-          System.out.println("Client socket closed");
-        }
-        if (serverSocket != null) {
-          serverSocket.close();
-          System.out.println("Server socket closed");
-        }
-      } catch (IOException e) {
-        System.out.println("IOException while closing sockets: " + e.getMessage());
-      }
-    }
+    //  Uncomment this block to pass the first stage
+     ServerSocket serverSocket = null;
+       Socket clientSocket = null;
+       int port = 6379;
+       try {
+         serverSocket = new ServerSocket(port);
+         // Since the tester restarts your program quite often, setting SO_REUSEADDR
+         // ensures that we don't run into 'Address already in use' errors
+         serverSocket.setReuseAddress(true);
+         // Wait for connection from client.
+         clientSocket = serverSocket.accept();
+       } catch (IOException e) {
+         System.out.println("IOException: " + e.getMessage());
+       } finally {
+         try {
+           if (clientSocket != null) {
+             clientSocket.close();
+           }
+         } catch (IOException e) {
+           System.out.println("IOException: " + e.getMessage());
+         }
+       }
   }
 }
